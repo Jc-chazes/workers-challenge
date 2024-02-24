a4cf7afc88feaed0be451f74367d49708c811521
jeancarlo.rojas <jeancarlorjs@gmail.com>
2024-02-24 10:34:10 -0500

    feat: add Checkbox component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,135 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react'
+import styled from 'styled-components'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 400px;
+  height: 200px;
+  margin: 0 auto;
+`
+
+export const Label = styled.label`
+  width: 200px;
+`
+
+enum CheckboxValue {
+  SELECTAll = 'SelectAll',
+  INDIA = 'India',
+  USA = 'USA',
+  FRANCE = 'France',
 }
 
-export default App;
+const arrCheckboxs = [
+  {
+    value: CheckboxValue.SELECTAll,
+    label: 'Select All',
+  },
+  {
+    value: CheckboxValue.INDIA,
+    label: 'India',
+  },
+  {
+    value: CheckboxValue.USA,
+    label: 'USA',
+  },
+  {
+    value: CheckboxValue.FRANCE,
+    label: 'France',
+  },
+]
+
+interface CheckboxProps {
+  value: string
+  name: string
+  isChecked: boolean
+  label: string
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+interface InitialCheckedItems {
+  SelectAll: boolean
+  India: boolean
+  USA: boolean
+  France: boolean
+}
+
+const initialCheckedItems: InitialCheckedItems = {
+  [CheckboxValue.SELECTAll]: false,
+  [CheckboxValue.INDIA]: false,
+  [CheckboxValue.USA]: false,
+  [CheckboxValue.FRANCE]: false,
+}
+
+function Checkbox({ value, name, isChecked, label, onChange }: CheckboxProps) {
+  return (
+    <Label>
+      <input
+        type="checkbox"
+        id={name}
+        name={name}
+        value={value}
+        checked={isChecked}
+        onChange={onChange}
+      />
+      {label}
+    </Label>
+  )
+}
+
+function getCheckboxParentChecked(checkedItems: InitialCheckedItems) {
+  return Object.keys(checkedItems)
+    .filter((item) => item !== CheckboxValue.SELECTAll)
+    .every((item) => checkedItems[item as keyof InitialCheckedItems])
+}
+
+export function App() {
+  const [checkedItems, setCheckedItems] = React.useState<InitialCheckedItems>(
+    initialCheckedItems,
+  )
+
+  const onChangeCheckboxValue = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    const { value, checked } = event.target
+
+    if (value === CheckboxValue.SELECTAll) {
+      setCheckedItems((items: InitialCheckedItems) =>
+        Object.keys(items).reduce((acc, key) => {
+          acc[key as keyof InitialCheckedItems] = checked
+          return acc
+        }, {} as InitialCheckedItems),
+      )
+      return
+    }
+
+    const newCheckedItems: InitialCheckedItems = {
+      ...checkedItems,
+      [value]: checked,
+    }
+
+    setCheckedItems({
+      ...newCheckedItems,
+      [CheckboxValue.SELECTAll]: getCheckboxParentChecked(newCheckedItems),
+    })
+  }
+
+  return (
+    <Container>
+      {arrCheckboxs.map((checkbox) => {
+        return (
+          <Checkbox
+            key={checkbox.value}
+            name={checkbox.value}
+            value={checkbox.value}
+            label={checkbox.label}
+            isChecked={checkedItems[checkbox.value]}
+            onChange={onChangeCheckboxValue}
+          />
+        )
+      })}
+    </Container>
+  )
+}
